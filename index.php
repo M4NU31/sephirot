ad33707e0fcfbc609c5c7ad92ad4c674a2835630
Manuel <frama9125@gmail.com>
2025-12-28 22:21:54 -0500

    update css

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ if(file_exists(__PATH_TEMPLATE_ROOT__ . 'inc/template.functions.php')){
   include(__PATH_TEMPLATE_ROOT__ . 'inc/template.functions.php');
 }
 
+$serverInfoCache = LoadCacheData('server_info.cache');
+if(is_array($serverInfoCache)) {
+	$srvInfo = explode("|", $serverInfoCache[1][0]);
+}
+
 $maxOnline = config('maximum_online', true);
 $onlinePlayers = check_value($srvInfo[3]) ? $srvInfo[3] : 0;
 $onlinePlayersPercent = check_value($maxOnline) ? $onlinePlayers*100/$maxOnline : 0;
