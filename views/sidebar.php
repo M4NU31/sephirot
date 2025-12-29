7b6b59dd975fd384afb7c22916971191481f2d64
Manuel <frama9125@gmail.com>
2025-12-28 21:28:57 -0500

    remove online function

--- views/sidebar.php
+++ views/sidebar.php
@@ -6,9 +6,6 @@
     </div>
     <div class="status">Version: <b>Season 8 Ep 3</b></div>
     <div class="status">Status: <b>Online</b></div>
-    <?php if( isServerOnline() ){
-      echo 'esta online';
-    } ?>
   </div>
 
   <a class="btn primary" style="width:100%;justify-content:center;margin-top:14px"
