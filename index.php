68a3d797c9d154aabfb7616b3389282fa1294e16
Manuel <frama9125@gmail.com>
2025-12-30 23:26:06 -0500

    re update slider

--- index.php
+++ index.php
@@ -21,6 +21,6 @@ if(is_array($serverInfoCache)) {
 $maxOnline = config('maximum_online', true);
 $onlinePlayers = check_value($srvInfo[3]) ? $srvInfo[3] : 0;
 $onlinePlayersPercent = check_value($maxOnline) ? $onlinePlayers*100/$maxOnline : 0;
-$css_version = '1.1.28';
+$css_version = '1.1.29';
 
 include(__PATH_TEMPLATE_ROOT__ . 'views/layout.php');
